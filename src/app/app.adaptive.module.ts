427307fbffa006db80947753db79399230490640
aperevalov <a.perevalov@artsofte.ru>
2018-11-10 18:25:35 +0500

    err

--- src/app/app.adaptive.module.ts
+++ src/app/app.adaptive.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AppAdaptiveComponent } from './app.adaptive.component';
-import { HomeAdaptiveComponent } from './home/home.adaptive.component';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule } from '@angular/router';
+// import { AppAdaptiveComponent } from './app.adaptive.component';
+// import { HomeAdaptiveComponent } from './home/home.adaptive.component';
 
 
-@NgModule({
-  declarations: [
-    AppAdaptiveComponent,
-    HomeAdaptiveComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
-    RouterModule.forRoot([
-      { path: '', component: HomeAdaptiveComponent, pathMatch: 'full'},
-      { path: 'lazy', loadChildren: './lazy/lazy.desktop.module#LazyModule'},
-      { path: 'lazy/nested', loadChildren: './lazy/lazy.desktop.module#LazyModule'}
-    ]),
-  ],
-  providers: [],
-  bootstrap: [AppAdaptiveComponent]
-})
-export class AppAdaptiveModule { }
+// @NgModule({
+//   declarations: [
+//     AppAdaptiveComponent,
+//     HomeAdaptiveComponent,
+//   ],
+//   imports: [
+//     BrowserModule.withServerTransition({appId: 'my-app'}),
+//     RouterModule.forRoot([
+//       { path: '', component: HomeAdaptiveComponent, pathMatch: 'full'},
+//       { path: 'lazy', loadChildren: './lazy/lazy.desktop.module#LazyModule'},
+//       { path: 'lazy/nested', loadChildren: './lazy/lazy.desktop.module#LazyModule'}
+//     ]),
+//   ],
+//   providers: [],
+//   bootstrap: [AppAdaptiveComponent]
+// })
+// export class AppAdaptiveModule { }
